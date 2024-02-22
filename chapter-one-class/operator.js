d1a663790bb7bdc16e649445947ed9d1a64b9156
ovijet <ovijethalder143@gmail.com>
2024-02-22 17:01:36 +0600

    javascript-class

---
+++ chapter-one-class/operator.js
@@ -0,0 +1,8 @@
+// // Type of operator
+
+// 1.Arithmetic  +,-,*,/,%
+// 2. Assignment =, +=,-=,*=,/=,%=,
+// 3. Relational >,<,<=,>=,==,===,!=
+// 4. Logical   &&,||,!
+// 5. Increment Decrement  ++,--
+// 6. Bitwise
