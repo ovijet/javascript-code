143007ea1b237cc9e245ab8414ede88e1c1c2bbe
ovijet <ovijethalder143@gmail.com>
2024-02-22 18:29:16 +0600

    javascript

--- chapter-tow-class/if.js
+++ chapter-tow-class/if.js
@@ -1,18 +1,27 @@
 let a = 10;
 let b = 20;
 if (a > b) {
-  console.log("b is big numbar"); 
+  console.log("b is big numbar");
 } else {
   console.log("a is big numbar");
 }
 
- let n = 5
-if (n % 2 ===0) {
-    console.log(n + 'even numbar');
+let n = 5;
+if (n % 2 === 0) {
+  console.log(n + "even numbar");
 }
 
+if (n % 2 === 1) {
+  console.log(n + "odd numbar");
+}
 
-if(n % 2 ===1){
-    console.log(n + 'odd numbar');
+let yourAge = 20;
+let yourSister = 25;
 
+if (yourAge < yourSister) {
+  console.log('you are elder');
+}else if(yourSister>yourAge){
+  console.log('you sister is elder');
+}else{
+  console.log('your both you r same');
 }
