d2af6fa134a6f669a84a5b862615c5eff1dea6c5
ovijet <ovijethalder143@gmail.com>
2024-02-24 18:14:27 +0600

    javascript

--- chapter-six/array.js
+++ chapter-six/array.js
@@ -6,6 +6,8 @@
 // console.log(arr[4]);
 // // console.log(arr.length);
 
+// console.log(arr.length);
+
 let arr = [1, 2, 3, 4, 5, 6];
 // arr[3] = 20;
 
@@ -13,13 +15,20 @@ console.log(arr.unshift());
 // console.log(arr[2]);
 // console.log(arr.length);
 
-// console.log(arr.pop());
+console.log(arr.pop());
+
+console.log(arr.length);
+
 
 for(let i=0;i<arr.length;i++){
-    if (arr[i]%2===0) {
-        console.log(arr[i]);
-    }
+    console.log(arr);
 }
 
+// for(let i=0;i<arr.length;i++){
+//     if (arr[i]%2===0) {
+//         console.log(arr[i]);
+//     }
+// }
 
-console.log( arr[2]);
+
+// console.log( arr[2]);
