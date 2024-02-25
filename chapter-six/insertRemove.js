9ecec87f6d36ad62b79a1a7a9b8a759ee4c10e5b
ovijet <ovijethalder143@gmail.com>
2024-02-25 18:37:25 +0600

    javascript-array

--- chapter-six/insertRemove.js
+++ chapter-six/insertRemove.js
@@ -13,7 +13,6 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 // arr.shift()
 // arr.splice(4,0,20)
 
-
 // arr.push(10)
 // arr.pop()
 // arr.unshift()
@@ -21,5 +20,33 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 // arr.splice(3,1)
 // arr.filter()
 
-arr.splice(3,0,10)
-console.log(arr); 
+// arr.splice(3,0,10)
+// console.log(arr);
+
+console.log(arr);
+
+let sum = 0;
+
+for (let i = 0; i < arr.length; i++) {
+    sum +=arr[i]
+}
+console.log(sum);
+
+
+// arrayr even numbar
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i]%2==0) {
+        console.log(arr[i]);
+    }
+    
+}
+
+
+// arrayr odd numbar
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i]%2==1) {
+        console.log(arr[i]);
+    }
+    
+}
