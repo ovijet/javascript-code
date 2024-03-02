1fcbd3874ce7ea2fa29ec4e404646c102f7a1829
ovijet <ovijethalder143@gmail.com>
2024-03-03 00:03:54 +0600

    function

--- chapter-eight/argument.js
+++ chapter-eight/argument.js
@@ -6,3 +6,20 @@ test(10, 20, 30, 60);
 for (let i = 0; i <= 10; i++) {
     console.log(i);
 }
+
+function name() {
+  let a = 10;
+  let b = 20;
+  let result = a + b;
+  console.log(result);
+}
+
+name()
+
+function sum(a, b) {
+  console.log(a + b);
+}
+sum(10, 10);
+sum(40, 10);
+sum(30, 10);
+sum(20, 10);
