6bee5b5930ae39cc929f16b751da8ba77b8e9ddc
ovijet <ovijethalder143@gmail.com>
2024-03-11 18:20:00 +0600

    scope

--- chapter-eight/scope.js
+++ chapter-eight/scope.js
@@ -9,10 +9,29 @@ let a = "abc";
 // console.log(b);
 
 function x() {
+  let a= 30
   function y() {
-    let a = 20;
+    let a = 30;
     console.log(a);
   }
   y();
 }
 x();
+
+
+function test(n){
+  function a(){
+    return n%3===0
+  }
+  function b(){
+    return n %5===0
+  }
+
+  if (a() && b()) {
+    console.log(n + ' is a divisible by both 3 and 5');
+  }else{
+    console.log('not a valid numbar');
+  }
+}
+
+test(15)
