d60bab727faad26d962ed3bc7e9d8be333889949
ovijet <ovijethalder143@gmail.com>
2024-03-17 18:49:12 +0600

    puref

--- chapter-nine/puref.js
+++ chapter-nine/puref.js
@@ -1,3 +1,14 @@
-for (let i = 0; i <= 5; i++) {
-  console.log(i);
+// for (let i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
+
+// pure function 
+
+function sqr(n){
+  return n * n
 }
+
+console.log(sqr(3));
+console.log(sqr(3));
+console.log(sqr(3));
