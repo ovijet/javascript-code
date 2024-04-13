743dcdede693055c5efc7f614ba4d7afc326aae3
ovijet <ovijethalder143@gmail.com>
2024-04-13 16:11:17 +0600

    practis

---
+++ chapter-nine/try.js
@@ -0,0 +1,19 @@
+let arr = [1,2,3,4,5,6,7,8,9,10]
+//  console.log(arr);
+
+// let sum = 0
+//  for(let i=0;i<arr.length;i++){
+//   sum +=arr[i]
+//  }
+
+//  console.log(result = sum )
+
+
+
+if (arr<11) {
+
+  console.log('false');
+  
+}else{
+  console.log('true');
+}
