ac56fbbfe04e43962ef69130f5e879dd8dd55d94
ovijet <ovijethalder143@gmail.com>
2024-07-10 10:47:12 +0600

    oob

--- oob-chapter-twelve/index.js
+++ oob-chapter-twelve/index.js
@@ -1,5 +1,17 @@
 // alert("i am a oob");
 
-for (let i = 0; i <= 1; i++) {
-  console.log(i);
-}
+let rect = {
+  Width: 100,
+  Height: 50,
+  draw: function () {
+    console.log("i am arectangle");
+    console.log("the width is " + this.Width);
+    console.log("the height is " + this.Height);
+  },
+};
+
+rect.draw();
+
+rect.Height = 200;
+
+rect.draw()
