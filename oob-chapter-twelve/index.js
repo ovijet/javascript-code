a3e86cfde4bf9f0655ebbbd1a8313e9eefa5c354
ovijet <ovijethalder143@gmail.com>
2024-07-12 18:29:17 +0600

    oob

--- oob-chapter-twelve/index.js
+++ oob-chapter-twelve/index.js
@@ -35,22 +35,22 @@
 
 // factory-patten in js--------------------------------//
 
-let creatrect = function (width, height) {
-  return {
-    Width: width,
-    Height: height,
-    draw: function () {
-      console.log("i am arectangle");
-      this.printProparty();
-      console.log(this);
-    },
+// let creatrect = function (width, height) {
+//   return {
+//     Width: width,
+//     Height: height,
+//     draw: function () {
+//       console.log("i am arectangle");
+//       this.printProparty();
+//       console.log(this);
+//     },
 
-    printProparty: function () {
-      console.log("the width is " + this.Width);
-      console.log("the height is " + this.Height);
-    },
-  };
-};
+//     printProparty: function () {
+//       console.log("the width is " + this.Width);
+//       console.log("the height is " + this.Height);
+//     },
+//   };
+// };
 
 // let rect1 = creatrect(20, 30);
 // rect1.draw();
@@ -60,21 +60,33 @@ let creatrect = function (width, height) {
 
 // constractor pattan ---------------------//
 
-let rectangle = function (width, height) {
-  this.Width = width;
-  this.Height = height;
-  this.draw = function () {
-    console.log("i am arectangle");
-    this.printProparty();
-    console.log(this);
-  };
+// let rectangle = function (width, height) {
+//   this.Width = width;
+//   this.Height = height;
+//   this.draw = function () {
+//     console.log("i am arectangle");
+//     this.printProparty();
+//     console.log(this);
+//   };
 
-  this.printProparty = function () {
-    console.log("the width is " + this.Width);
-    console.log("the height is " + this.Height);
-  };
-};
+//   this.printProparty = function () {
+//     console.log("the width is " + this.Width);
+//     console.log("the height is " + this.Height);
+//   };
+// };
 
-let rect3 = new rectangle(2,3);
+// let rect3 = new rectangle(2,3);
 
-rect3.draw();
+// rect3.draw();
+
+function myFun(c, d) {
+  console.log(this);
+  console.log(this.a + this.b + c + d);
+}
+
+myFun.call({ a: 30, b: 40 });
+myFun.apply({ a: 30, b: 40 }, [10, 20]);
+
+let bindd = myFun.bind({ a: 7, b: 3 });
+
+bindd(5,5)
