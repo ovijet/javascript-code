467b7b852616fcdda56974a582fe0c549f228c60
ovijet <ovijethalder143@gmail.com>
2024-08-18 17:25:45 +0600

    inheritense

---
+++ chapter-thirteen/index.js
@@ -0,0 +1,18 @@
+// // Inheritance
+
+// function person(name) {
+//   this.name = name;
+// }
+// let p1 = new person("ovijet halder");
+
+// console.log(p1);
+
+let person = {
+  name: "ovijet halder",
+};
+
+// let desk = Object.getOwnPropertyDescriptor(person, "name");
+
+// console.log(desk);
+
+let past = Object.getPrototypeOf(person);
