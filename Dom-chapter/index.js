be827133ef36329dc532c45dd0622b5f6a1d5ff2
ovijet <ovijethalder143@gmail.com>
2024-10-04 22:40:30 +0600

    dom

---
+++ Dom-chapter/index.js
@@ -0,0 +1,23 @@
+// import "./lesson-1.js";
+
+// const { query } = require("express");
+
+// 1.getElementById
+// 2.getElementsByClassName
+// 3.getElementsByTagName
+
+// let title = document.getElementById("title");
+// let lead = document.getElementsByClassName("lead");
+// let a = document.getElementsByTagName("a");
+
+// console.log(title);
+// console.log(lead);
+// console.log(a);
+
+// queryseletor
+
+let title = document.querySelector("#title");
+console.log(title);
+
+let lead = document.querySelectorAll(".lead");
+console.log(lead);
